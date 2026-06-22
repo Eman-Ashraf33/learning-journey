1761e8ff7cf50a56804181bc15fc044671075ace
Eman <emy0861520@gmail.com>
2026-06-23 00:48:16 +0300

    challenge 1

--- Working with Arrays/script.js
+++ Working with Arrays/script.js
@@ -156,3 +156,21 @@ currenciesUnique.forEach(function (value, _, map) {
 });
 
 */
+
+//challenge 1
+
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliaCorrected = dogsJulia.slice(1, -2);
+  const doges = dogsJuliaCorrected.concat(dogsKate);
+  doges.forEach(function (dog, i) {
+    if (dog >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy`);
+    }
+  });
+};
+
+const julia1 = [3, 5, 2, 12, 7];
+const kate1 = [4, 1, 15, 8, 3];
+checkDogs(julia1, kate1);
