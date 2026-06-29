594a52380fdf0773dac35df4668b3eb37326aea8
Eman <emy0861520@gmail.com>
2026-06-29 22:36:53 +0300

    add smooth scrolling to section 1 and log viewport dimensions

--- 13- Advanced DOM and Events/script.js
+++ 13- Advanced DOM and Events/script.js
@@ -99,4 +99,14 @@ const section1 = document.querySelector('#section--1');
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
   console.log(s1coords);
+  console.log('current scroll (x/y)', Window.pageYOffset, pageXOffset);
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth,
+  );
+
+  //Scrolling
+  //window.scrollTo(s1coords.left,s1coords.top);
+  section1.scrollIntoView({ behavior: 'smooth' });
 });
