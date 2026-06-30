8dc795d34d0f091e2f7a11a22fed90a1cf4c979f
Eman <emy0861520@gmail.com>
2026-06-30 22:19:05 +0300

    add mouseenter event handler to h1 for alert and remove after 3 seconds

--- 13- Advanced DOM and Events/script.js
+++ 13- Advanced DOM and Events/script.js
@@ -110,3 +110,15 @@ btnScrollTo.addEventListener('click', function (e) {
   //window.scrollTo(s1coords.left,s1coords.top);
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+// Types of Events and Event Handlers
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('onmouseenter  : Great! you are reading the heading');
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
