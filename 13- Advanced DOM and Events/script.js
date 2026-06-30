13639bbe7b16f3639ca9d0fbdee33c1d2a30accd
Eman <emy0861520@gmail.com>
2026-06-30 23:57:50 +0300

    add event propagation examples and random color functionality to navigation links

--- 13- Advanced DOM and Events/script.js
+++ 13- Advanced DOM and Events/script.js
@@ -112,7 +112,7 @@ btnScrollTo.addEventListener('click', function (e) {
 });
 
 // Types of Events and Event Handlers
-
+/*
 const h1 = document.querySelector('h1');
 
 const alertH1 = function (e) {
@@ -122,3 +122,31 @@ const alertH1 = function (e) {
 h1.addEventListener('mouseenter', alertH1);
 
 setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+*/
+
+//Event Propagation in Practice
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('Link', e.target, e.currentTarget);
+  console.log(e.currentTarget == this);
+
+  //stop propagation
+  e.stopPropagation();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('container', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('nav', e.target, e.currentTarget);
+});
